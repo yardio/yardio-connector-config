ce77c741d527db3de50654b4006b5068b92f1be0
Paul Dijou <paul.dijou@gmail.com>
2014-07-19 13:08:39 +1200

    Improve config

--- src/main/scala/ConfigConnector.scala
+++ src/main/scala/ConfigConnector.scala
@@ -5,14 +5,16 @@ import scala.concurrent.Future
 import play.api.Configuration
 import play.api.libs.json.{Json, Reads, Writes}
 
-import io.yard.models._
+import io.yard.common.models._
 
 object ConfigConnector extends io.yard.connector.api.Connector {
-  def read[T <: ProviderConfiguration](organization: Organization, provider: Provider)(implicit reader: Reads[T]): Future[Option[T]] = Future.successful(reader.reads(Json.parse("{}")).asOpt)
+  def orgs(implicit reader: Reads[Organization]) = Seq(Organization("movio"))
 
-  def save[T <: ProviderConfiguration](organization: Organization, provider: Provider)(implicit writer: Writes[T]): Future[Boolean] = Future.successful(true)
+  def read[T <: ProviderConfiguration](provider: Provider, organization: Option[Organization])(implicit reader: Reads[T]): Future[Option[T]] = Future.successful(reader.reads(Json.parse("{}")).asOpt)
 
-  def read[T <: ModuleConfiguration](organization: Organization, module: Module)(implicit reader: Reads[T]): Future[Option[T]] = Future.successful(reader.reads(Json.parse("{}")).asOpt)
+  def save[T <: ProviderConfiguration](provider: Provider, organization: Option[Organization])(implicit writer: Writes[T]): Future[Boolean] = Future.successful(true)
 
-  def save[T <: ModuleConfiguration](organization: Organization, module: Module)(implicit writer: Writes[T]): Future[Boolean] = Future.successful(true)
+  def read[T <: ModuleConfiguration](module: Module, organization: Option[Organization])(implicit reader: Reads[T]): Future[Option[T]] = Future.successful(reader.reads(Json.parse("{}")).asOpt)
+
+  def save[T <: ModuleConfiguration](module: Module, organization: Option[Organization])(implicit writer: Writes[T]): Future[Boolean] = Future.successful(true)
 }
--- src/main/scala/ConfigConnectorPlugin.scala
+++ src/main/scala/ConfigConnectorPlugin.scala
@@ -2,7 +2,7 @@ package io.yard.connector.config
 
 import io.yard.module.core.Api
 
-class ConfigConnectorConfig(application: play.api.Application) extends play.api.Plugin {
+class ConfigConnectorPlugin(application: play.api.Application) extends play.api.Plugin {
   override def onStart = {
     Api.registerConnector(ConfigConnector)
   }
